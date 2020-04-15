767e0557c20d31ddb109bd20207f89e01fdfeb03
biner <huanghuibin@gmail.com>
2020-04-15 20:31:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-if [ -z "$INPUT_ARGS" ]; then
-  echo 'Required Args parameter'
-  exit 1
-fi
 
 curl -fsSL git.io/file-transfer | sh          
 echo "Running command: ./transfer $platform $path $flags"
