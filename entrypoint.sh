d16e5020f4e04105675548c4463cf3141dd583ca
biner <huanghuibin@gmail.com>
2020-04-16 11:48:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-
-
+RUN curl -sL https://git.io/file-transfer | sh 
 
 echo "Running command: ./transfer $INPUT_PLATFORM $INPUT_PATH $INPUT_FLAGS"
 ./transfer $INPUT_PLATFORM $INPUT_PATH $INPUT_FLAGS  2>&1 |grep -oP 'http.*'| tee uri.log
