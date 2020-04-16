61988aa84c033f06ea8c141cca72f0cf1ea02d19
biner <huanghuibin@gmail.com>
2020-04-16 11:24:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,7 @@ curl -sL https://git.io/file-transfer | sh
 
 
 echo "Running command: ./transfer $INPUT_PLATFORM $INPUT_PATH $INPUT_FLAGS"
-./transfer $INPUT_PLATFORM $INPUT_PATH $INPUT_FLAGS  2>&1 | tee uri.log
-echo "Running command END: ./transfer $INPUT_PLATFORM $INPUT_PATH $INPUT_FLAGS"
+./transfer $INPUT_PLATFORM $INPUT_PATH $INPUT_FLAGS  2>&1 |grep -oP 'http.*'| tee uri.log
 
-echo "::warning file=wetransfer.com::$(cat uri.log | grep https)"
-
-uri=$(cat uri.log | grep https)
+uri=$(cat uri.log)
 echo ::set-output name=uri::$uri
